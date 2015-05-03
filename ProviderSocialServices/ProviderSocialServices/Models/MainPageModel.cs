2746c4ab5533493d4955059fe349bd76b97e3f47
AndreyMisnikov <andrey9_5@mail.ru>
2015-05-04 02:00:48 +0300

    First Build

---
+++ ProviderSocialServices/ProviderSocialServices/Models/MainPageModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProviderSocialServices.Models
+{
+    public class MainPageModel
+    {
+        public class Organization
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class Activity
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class AvailableService
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class CategoryOfClient
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class GeographyOfActivity
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class OrganizationLegalForm
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public IList<Organization> Organizations { get; set; }
+        public IList<Activity> Activities { get; set; }
+        public IList<AvailableService> AvailableServices { get; set; }
+        public IList<CategoryOfClient> CategoryOfClients { get; set; }
+        public IList<GeographyOfActivity> GeographyOfActivities { get; set; }
+        public IList<OrganizationLegalForm> OrganizationLegalForms { get; set; }
+
+    }
+}
---
+++ ProviderSocialServices/ProviderSocialServices/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProviderSocialServices.Startup))]
+namespace ProviderSocialServices
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
