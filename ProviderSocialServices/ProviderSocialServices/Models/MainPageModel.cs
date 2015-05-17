9fbb6c5a48812f61da067b16d187e4437afcd4ab
AndreyMisnikov <andrey9_5@mail.ru>
2015-05-17 13:53:52 +0300

    List Organization(bootstrap)

--- ProviderSocialServices/ProviderSocialServices/Models/MainPageModel.cs
+++ ProviderSocialServices/ProviderSocialServices/Models/MainPageModel.cs
@@ -2,47 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ProviderSocialServices.ObjectBD;
 
 namespace ProviderSocialServices.Models
 {
     public class MainPageModel
     {
-        public class Organization
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-        }
-
-        public class Activity
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-        }
-
-        public class AvailableService
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-        }
-
-        public class CategoryOfClient
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-        }
-
-        public class GeographyOfActivity
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-        }
-
-        public class OrganizationLegalForm
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-        }
-
         public IList<Organization> Organizations { get; set; }
         public IList<Activity> Activities { get; set; }
         public IList<AvailableService> AvailableServices { get; set; }
